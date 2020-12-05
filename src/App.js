4bf08f24f8f60228d539d248532dfd8c39fe7531
Harshith-KN <harshithkn98@gmail.com>
2020-12-05 21:44:20 +0530

    Added product information screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+
 function App() {
   return (
+      <BrowserRouter>
     <div className="grid-container">
             <header className="row">
                 <div>
@@ -14,44 +19,10 @@ function App() {
                 </div>
             </header>
             <main>
+                <Route path="/product/:id" component={ProductScreen}></Route>
+                <Route path="/" component={HomeScreen} exact></Route>
 
-                <div className="row center">
-                    {
-                      data.products.map((product) => (
-                      <div key={product._id} className="card">
-                      <a href={`/product/${product._id}`}>
-                          <img className="medium" src={product.image} alt={product.name}/>
-                      </a>
-                  <div className="card-body">
-                      <a href={`/product/${product._id}`}>
-                          <h2>{product.name}</h2>
-                      </a>
-                      <div className="rating">
-                          <span>
-                              <i className="fa fa-star"></i>
-                          </span>
-                          <span>
-                              <i className="fa fa-star"></i>
-                          </span>
-                          <span>
-                              <i className="fa fa-star"></i>
-                          </span>
-                          <span>
-                              <i className="fa fa-star"></i>
-                          </span>
-                          <span>
-                              <i className="fa fa-star-half-o"></i>
-                          </span>
-                      </div>
-                      <div className="price">
-                        ₹{product.price}
-                      </div>
-                  </div>
-                  </div> )
-                        )
-                    }
-                    
-                </div>
+                
             </main>
             <footer className="row center">
                 &#169;
@@ -59,6 +30,7 @@ function App() {
                 &#8482;
             </footer>
         </div>
+        </BrowserRouter>
   );
 }
 
